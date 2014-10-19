45c72abbdd930015c75da2ca04f19777a1fa7de6
borischernov <boris@imode.lv>
2014-10-19 03:17:10 +0300

    fix app_folder initialization

--- lib/trustlink.rb
+++ lib/trustlink.rb
@@ -13,6 +13,7 @@ module Trustlink
       
       opts = config[Rails.env]['options'] || {}
       opts.merge!(extra_options)
+      opts[:app_folder] ||= Rails.root
       opts[:template_path] ||= File.absolute_path(File.join(File.dirname(__FILE__),'..','template','template.tpl.html'))
       TlClient.new(config[Rails.env]['user_id'], request, opts)
     end
